d8f3103a366f19a883623bddcf2978227c51d43b
heleyang <heleyang0311@163.com>
2023-12-06 22:57:11 +0800

    用户登录接口

--- entry/src/main/ets/data/data.d.ts
+++ entry/src/main/ets/data/data.d.ts
@@ -1,11 +1,37 @@
 // 约定axios的请求返回response的格式
 
-export type UserProfile = {
+export type UserProfileResponse = {
   id: string
   avatar: string
   name: string
   phoneNum: string
   description: string
   gallery: string[]
+}
+
+export type LoginRequest = {
+  email: string
+  password: string
+}
+
+export type LoginResponse = {
+  msg: string;
+  saTokenInfo: SaTokenInfo;
+  [property: string]: any;
+}
+
+export interface SaTokenInfo {
+  isLogin: boolean;
+  loginDevice: string;
+  loginId: string;
+  loginType: string;
+  sessionTimeout: number;
+  tag: null;
+  tokenActiveTimeout: number;
+  tokenName: string;
+  tokenSessionTimeout: number;
+  tokenTimeout: number;
+  tokenValue: string;
+  [property: string]: any;
 }
 
