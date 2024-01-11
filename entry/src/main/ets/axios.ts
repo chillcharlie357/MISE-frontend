050706911b11b344e7a7679f8879ff1ecd10eba7
heleyang <heleyang0311@163.com>
2024-01-11 20:54:34 +0800

    fix： axios.ts and Index.ets

--- entry/src/main/ets/axios.ts
+++ entry/src/main/ets/axios.ts
@@ -3,7 +3,7 @@ import axios from '@ohos/axios'
 
 const instance = axios.create(
   {
-    baseURL: 'http://172.31.27.189:4523/m1/3686963-0-default'
+    baseURL: 'http://172.31.1.179:8080'
   }
 )
 
